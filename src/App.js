d1f69f3d135ddb686d06ece8845e7e2f7eb444ca
Brandon Biggs <brandonkeithbiggs@gmail.com>
2017-12-02 06:40:25 -0800

    Added the settings for the router to work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Journal from './wcag_journal'
 
+function Home(){
+	return <p>This is home</p>
+}
+
+function PageNotFound(){
+	return <p>Sorry, there is no page at this URL</p>
+}
 
 class App extends Component {
 	constructor(props){
@@ -14,7 +22,12 @@ class App extends Component {
 	render() {
 		return (
 		<div>
-		<Journal />
+
+		<Switch>
+		<Route exact path="/" component={Home} />
+		<Route path="/journal" component={Journal} />
+		<Route component={PageNotFound} />
+		</Switch>
 			</div>
 		);
 	}
---
+++ src/wcag_journal/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+export default function(props){
+	return <p>Hello world</p>
+}
